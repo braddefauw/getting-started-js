a47cbd34f62665133b4b2aa41c74131a8ce730e2
Brad DeFauw <badefauw@gmail.com>
2023-10-18 15:37:49 -0500

    get a list of all recipes

--- app.js
+++ app.js
@@ -12,6 +12,17 @@ db.serialize( () => {
     db.run("CREATE TABLE IF NOT EXISTS recipes (id INTEGER PRIMARY KEY, name TEXT, ingredients TEXT, instructions TEXT)")
 })
 
+// get (read) a list of all recipes
+app.get('/recipes', (req, res) => {
+    db.all('SELECT * FROM recipes', (err, rows) => {
+        if(err){
+            res.status(500).json({error: err.message});
+            return;
+        }
+        res.json(rows);
+    })
+})
+
 // start the server
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
